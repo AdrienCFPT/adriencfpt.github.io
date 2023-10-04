1e748fe97b8995a683cabfea0012b5475c7ffa1b
AdrienCFPT <adrien.gvn@eduge.ch>
2023-10-04 14:00:04 +0200

    v.1.4

--- main.js
+++ main.js
@@ -1,66 +1,13 @@
-async function PrendreAllCountry() {
+console.log("Local Storage");
 
-    const myInit = {
-        method: 'GET',
-        headers: {},
-        mode: 'cors',
-        cache: 'no-cache',
-    }
-  
-    let response = await fetch("https://restcountries.com/v3.1/all", myInit);
-  
-    if (response.ok) {
-        let text = await response.text();
-        return JSON.parse(text);
-    }
-    else {
-        let text = await response.text();
-        throw new Error(`${response.status} : ${text}`);
-    }
-  }
-  
+let allCountry = JSON.parse(localStorage.getItem("allCountry"));
 
-window.addEventListener("load", (event) => {
+allCountry.forEach(element => {
+    lstname.push(element["name"]["common"]);
+})
+lstname.sort();
 
-    let lstname = [];
-    let html = "";
-
-    let timeFetch = Date.now() - localStorage.getItem("date");
-    let DifferenceInDays = timeFetch / (1000 * 3600 * 24);
-
-    if(localStorage.getItem("allCountry") == null || DifferenceInDays > 30) {
-
-    console.log("Requete à l'API");
-
-    PrendreAllCountry()
-        .then(resultat => {
-            resultat.forEach(element => {
-                lstname.push(element["name"]["common"]);
-            })
-            lstname.sort();
-
-            lstname.forEach(element => {
-                html += element + " - ";
-            })
-        }
-        )
-        .catch(error => alert(error))
-    }
-    else {
-
-        console.log("Local Storage");
-
-        let allCountry = JSON.parse(localStorage.getItem("allCountry"));
-
-        allCountry.forEach(element => {
-            lstname.push(element["name"]["common"]);
-        })
-        lstname.sort();
-
-        lstname.forEach(element => {
-            html += element + " - ";
-        })
-        document.querySelector("body").innerHTML = html;
-    }
-});
-
+lstname.forEach(element => {
+    html += element + " - ";
+})
+document.querySelector("body").innerHTML = html;
