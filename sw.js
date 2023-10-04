3666376f021b80f7a53695c42d9eea793ebf9f0d
AdrienCFPT <adrien.gvn@eduge.ch>
2023-10-04 13:51:06 +0200

    v.1.2

--- sw.js
+++ sw.js
@@ -1,20 +1,10 @@
-async function PrendreAllCountry() {
-
-  const myInit = {
-      method: 'GET',
-      headers: {},
-      mode: 'cors',
-      cache: 'no-cache',
-  }
-
-  let response = await fetch("https://restcountries.com/v3.1/all", myInit);
-
-  if (response.ok) {
-      let text = await response.text();
-      return JSON.parse(text);
-  }
-  else {
-      let text = await response.text();
-      throw new Error(`${response.status} : ${text}`);
-  }
-}
+self.addEventListener("fetch", (e) => {
+    console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+        e.then(resultat => {
+            localStorage.clear();
+            localStorage.setItem("allCountry", JSON.stringify(resultat));
+            localStorage.setItem("date", Date.now());
+        }
+        )
+        .catch(error => alert(error))
+   });
