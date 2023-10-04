2b61dbb37d3842f1071b9b3e5ab4d98c18d4b1c3
AdrienCFPT <adrien.gvn@eduge.ch>
2023-10-04 13:24:34 +0200

    Project

---
+++ main.js
@@ -0,0 +1,69 @@
+async function PrendreAllCountry() {
+
+    const myInit = {
+        method: 'GET',
+        headers: {},
+        mode: 'cors',
+        cache: 'no-cache',
+    }
+
+    let response = await fetch("https://restcountries.com/v3.1/all", myInit);
+
+    if (response.ok) {
+        let text = await response.text();
+        return JSON.parse(text);
+    }
+    else {
+        let text = await response.text();
+        throw new Error(`${response.status} : ${text}`);
+    }
+}
+
+window.addEventListener("load", (event) => {
+
+    let lstname = [];
+    let html = "";
+
+    let timeFetch = Date.now() - localStorage.getItem("date");
+    let DifferenceInDays = timeFetch / (1000 * 3600 * 24);
+
+    if(localStorage.getItem("allCountry") == null || DifferenceInDays > 30) {
+
+    console.log("Requete à l'API");
+
+    PrendreAllCountry()
+        .then(resultat => {
+            resultat.forEach(element => {
+                lstname.push(element["name"]["common"]);
+            })
+            lstname.sort();
+
+            lstname.forEach(element => {
+                html += element + " - ";
+            })
+            localStorage.clear();
+            localStorage.setItem("allCountry", JSON.stringify(resultat));
+            localStorage.setItem("date", Date.now());
+            document.querySelector("body").innerHTML = html;
+        }
+        )
+        .catch(error => alert(error))
+    }
+    else {
+
+        console.log("Local Storage");
+
+        let allCountry = JSON.parse(localStorage.getItem("allCountry"));
+
+        allCountry.forEach(element => {
+            lstname.push(element["name"]["common"]);
+        })
+        lstname.sort();
+
+        lstname.forEach(element => {
+            html += element + " - ";
+        })
+        document.querySelector("body").innerHTML = html;
+    }
+});
+
